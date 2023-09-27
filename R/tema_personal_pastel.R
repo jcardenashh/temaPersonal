2914144e51e142687e7a33e2eba0b463abafc661
jcardenashh <60513883+jcardenashh@users.noreply.github.com>
2023-09-27 10:29:30 -0600

    Update tema_personal_pastel creación del ejemplo

--- R/tema_personal_pastel.R
+++ R/tema_personal_pastel.R
@@ -1,26 +1,31 @@
 
-
 #' Tema para gráficos de tipo pastel y donas
 
 #' @description Función para aplicar tema a graficos de pastel, elimina los ejes de un gráfico convencional
 
 #' @return
 #' @export
-#'
-#' @examples tema_personal_pastel(grafico_donus)
-#'
+
+#' @examples
+#' df = data.frame( categorias = c("cat1","cat2","cat3","cat4"), porcentaje = c(25,20,15,40))
+#' ggplot(df,aes(x = "",y = porcentaje, fill = categorias))+
+#'   geom_bar(stat = "identity",color="white")+
+#'   coord_polar(theta="y") +
+#'   tema_personal_pastel()
+
 #' @importFrom ggplot2 theme
 tema_personal_pastel <- function(){
 
   tema_personal() +
-  ggplot2::theme(legend.position = "right",
-          line = element_blank(),
-          rect = element_blank(),
-          axis.line=element_blank(),
-          axis.ticks = element_blank(),
-          axis.text.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.title = element_blank(),
-          panel.grid.major  = element_blank()
+    ggplot2::theme(legend.position = "right",
+                   line = element_blank(),
+                   rect = element_blank(),
+                   axis.line=element_blank(),
+                   axis.ticks = element_blank(),
+                   axis.text.x = element_blank(),
+                   axis.text.y = element_blank(),
+                   axis.title = element_blank(),
+                   panel.grid.major  = element_blank()
     )
 }
+

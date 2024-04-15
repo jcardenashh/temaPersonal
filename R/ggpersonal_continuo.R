bf2872be5f830c8f8a9ee2a1e4397fbb7704772d
jcardenashh <60513883+jcardenashh@users.noreply.github.com>
2024-04-15 09:56:07 -0600

    agregar carga de paquetes a los ejemplos

--- R/ggpersonal_continuo.R
+++ R/ggpersonal_continuo.R
@@ -1,5 +1,5 @@
 
-#' configurar gráficos continuos, coordenadas y colores
+#' configurar graficos continuos, coordenadas y colores
 #'
 #' @param grafico object " Grafico a personalizar"
 #'
@@ -7,13 +7,15 @@
 #' @export
 #'
 #' @examples
-#' grafico_lineas = ggplot(economics, aes(date, unemploy)) + geom_line()
+#' library(ggplot2,temaPersonal)
+#' theme_set(tema_personal())
+#' grafico_lineas = ggplot(economics, aes(date, unemploy)) + geom_line(color="tomato")
 #' ggpersonal_continuo(grafico_lineas)
 ggpersonal_continuo <- function(grafico){
   # para graficos variables continuas
   grafico+
-    ggplot2::scale_y_continuous(expand  =  expand_scale ( mult  =  c(0,0.1)),labels = scales::label_comma())+
-    ggplot2::scale_x_continuous(expand  =  expand_scale ( mult  =  c(0,0.1)),labels = scales::label_comma()) +
+    ggplot2::scale_y_continuous(expand  =  expansion ( mult  =  c(0,0.1)),labels = scales::label_comma())+
+    ggplot2::scale_x_continuous(expand  =  expansion ( mult  =  c(0,0.1)),labels = scales::label_comma()) +
     ggthemes::scale_color_tableau()+
     ggthemes::scale_fill_tableau()+
     ggplot2::guides(colour = guide_legend(override.aes = list(size=3))) # tamaño simbolos leyendas
